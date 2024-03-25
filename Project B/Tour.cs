e2d2f847348b2c159ce65550f14fd0d0611808fe
Job Limbeek <Job.Limbeek@gmail.com>
2024-03-25 09:22:09 +0100

    Add a limit to visitors per tour

--- Project B/Tour.cs
+++ Project B/Tour.cs
@@ -4,22 +4,51 @@ class Tour
 
     public DateTime tourStartTime;
     public int parttakers;
+    public bool opentourspots;
 
     public Tour(DateTime time)
     {
         tourStartTime = time;
         parttakers = 0;
+        opentourspots = true;
     }
 
     public void AddVisitor()
     {
-        parttakers++;
+        if (opentourspots == true)
+        {
+            parttakers++;
+            MaxVisitor();
+        }
+        else
+        {
+            Console.WriteLine("Maximum tourcapacity reached");
+        }
     }
 
     public void RemoveVisitor()
     {
-        parttakers--;
+        if (parttakers == 13)
+        {
+            parttakers--;
+            MaxVisitor();
+        }
+        else
+        {
+            parttakers--;
+            MaxVisitor();
+        }
     }
 
-
+    public void MaxVisitor()
+    {
+        if (parttakers < 3)
+        {
+            opentourspots = true;
+        }
+        else 
+        {
+            opentourspots = false;
+        }
+    }
 }
