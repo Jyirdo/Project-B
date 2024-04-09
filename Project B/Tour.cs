f1fd4c034cf52bd3a52f138f58022811bbc0cf9c
Job Limbeek <Job.Limbeek@gmail.com>
2024-04-09 20:58:54 +0200

    Add more tours

--- Project B/Tour.cs
+++ Project B/Tour.cs
@@ -4,14 +4,10 @@ class Tour
     public List<Visitor> visitorsintour = new();
     public int tour_id;
     public DateTime tourStartTime;
-    public int parttakers;
-    public bool opentourspots;
 
     public Tour(int id, DateTime time)
     {
         tour_id = id;
         tourStartTime = time;
-        parttakers = 0;
-        opentourspots = true;
     }
 }
