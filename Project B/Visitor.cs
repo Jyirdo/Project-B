fc519d9c8a8317aae7df5de19eeb2eb5e7b5d54f
Mprgx <1079546@hr.nl>
2024-04-09 11:08:25 +0200

    Add json writing and reading functions

--- Project B/Visitor.cs
+++ Project B/Visitor.cs
@@ -2,10 +2,12 @@ class Visitor
 {
     public long ticketID;
     public DateTime tourTime;
+    public int tourNumber;
 
-    public Visitor(long ticketid, DateTime tourtime)
+    public Visitor(long ticketid, DateTime tourtime, int tournumber)
     {
         ticketID = ticketid;
         tourTime = tourtime;
+        tourNumber = tournumber;
     }
 }
