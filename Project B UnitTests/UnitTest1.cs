bc4fefdb7ad2ca9781758e88686d19ff9192bf86
Axel <1059015@hr.nl>
2024-05-22 13:55:37 +0200

    Add visitor unit test

--- Project B UnitTests/UnitTest1.cs
+++ Project B UnitTests/UnitTest1.cs
@@ -1,4 +1,6 @@
 namespace Project_B_UnitTests;
+using NUnit.Framework;
+using System;
 
 [TestClass]
 public class UnitTest1
@@ -6,5 +8,17 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
+    }
+    public void VisitorTest()
+    {
+        long expectedBarcode = 1234567890;
+        DateTime expectedTourTime = new DateTime(2024, 5, 22, 14, 30, 0);
+        int expectedTourNumber = 1;
+
+        Visitor visitor = new Visitor(expectedBarcode, expectedTourTime, expectedTourNumber);
+
+        Assert.AreEqual(expectedBarcode, visitor.barcode);
+        Assert.AreEqual(expectedTourTime, visitor.tourTime);
+        Assert.AreEqual(expectedTourNumber, visitor.tourNumber);
     }
 }
